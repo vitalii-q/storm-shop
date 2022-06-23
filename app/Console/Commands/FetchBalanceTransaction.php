216b34e1da0ab55444279c2864fcf0ca545409d6
VitalyWeb <vitalicheg28@mail.ru>
2022-06-23 22:09:14 +0300

    console command

---
+++ app/Console/Commands/FetchBalanceTransaction.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Product;
+use Illuminate\Console\Command;
+
+class FetchBalanceTransaction extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'fetch:price {product_id? : ID продукта}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch product price from db';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $id = $this->argument('product_id'); // указываеый в команде аргумент
+        if (!$id) {
+            $id = $this->ask('Введите ID'); // вывод в консоль вопроса
+        }
+
+        $product = Product::find($id);
+        if(!$product) {
+            $this->error('Продукта с id ' . $id .' не существует'); // вывод ошибки
+            return 1;
+        }
+
+        print_r($product->price.PHP_EOL); // PHP_EOL перенос строки
+
+        return 0;
+    }
+}
